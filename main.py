b48a786c1a6c2652ee1cde99fbff751ea303a98e
Anastasia Samilyk <asamilyk@yandex.ru>
2025-02-07 13:05:13 +0300

    feat: add authentification, base pages and code for db connection

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+import psycopg2
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class pgsql_conn(object):
+
+    def __init__(self, host_name, port_name, database_name, user_name, password_name):
+        self.connection = psycopg2.connect(host=host_name,
+                                           port=port_name,
+                                           database=database_name,
+                                           user=user_name,
+                                           password=password_name)
+
+    def read_sql(self, sql_string):
+        with self.connection:
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql_string)
+                output = cursor.fetchall()
+                col_names = [desc[0] for desc in cursor.description]
+
+        return pd.DataFrame(output, columns=col_names)
+
+    def read_sql_ddl(self, sql_string):
+        with self.connection:
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql_string)
+                self.connection.commit()
+
+    def read_sql_dml(self, sql_string):
+        with self.connection:
+            with self.connection.cursor() as cursor:
+                cursor.execute(sql_string)
+                self.connection.commit()
+
+    def upload_from_file(self, f_path, sql_string):
+        with self.connection:
+            with self.connection.cursor() as cursor:
+                with open(f_path) as f:
+                    # cursor.copy_from(f, table_name, columns=column_names, sep=',')
+                    cursor.copy_expert(sql_string, f)
+
+                self.connection.commit()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+conn = pgsql_conn('127.0.0.1',
+                  5433,
+                  'cardio',
+                  'apsamilyk',
+                  '0502hsecardio2025')
 
+df = conn.read_sql(f''' 
+SELECT  
+    id, orig_id, chrom, pos  
+FROM crd_dmt.data_vcf 
+LIMIT 10
+''')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(df)
--- monitoring_genomic_mutations/settings.py
+++ monitoring_genomic_mutations/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
+    'authentication.apps.AuthenticationConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'monitoring_genomic_mutations.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'monitoring_genomic_mutations.wsgi.application'
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -116,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
